73737cf6505e47363bbdebb010df01dd796e7cd5
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-09-22 18:36:03 -0400

    Add TSLint, remove User entity and controller

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,19 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
+import * as bodyParser from "body-parser";
 import * as compression from "compression";
 import * as express from "express";
-import * as bodyParser from "body-parser";
 import * as morgan from "morgan";
-import {User} from "./entity/User";
-import { summarizeAllVotes, createVote, getVoteDetails, processBallot } from "./controller/VoteController";
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import { createVote, getVoteDetails, processBallot, summarizeAllVotes } from "./controller/VoteController";
 import { createVoter } from "./controller/VoterController";
 
 const version = "v0.1";
-const port = process.env["PORT"] || 3001;
+const port = process.env.PORT || 3001;
 
-createConnection().then(async connection => {
-
+createConnection().then(async (connection) => {
     // create express app
     const app = express();
-    
+
     // setup express app
     app.use(bodyParser.json());
     app.use(morgan("tiny"));
@@ -37,19 +35,6 @@ createConnection().then(async connection => {
 
     // start express server
     app.listen(port);
-
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
-
     console.log(`Express server has started on port ${port}. Open http://localhost:${port}/users to see results`);
 
-}).catch(error => console.log(error));
+}).catch((error) => console.log(error));
