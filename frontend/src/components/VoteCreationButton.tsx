489d2446204bcf73d0978649b411182a02ec9852
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-10-04 00:24:50 -0400

    Color VoteCreationButton

--- frontend/src/components/VoteCreationButton.tsx
+++ frontend/src/components/VoteCreationButton.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 export const VoteCreationButton: SFC<{}> = () => {
     return (
         <div style={{textAlign: "center"}}>
-            <Button variant="contained" size="large">
+            <Button variant="contained" size="large" color="primary">
                 Create Vote
             </Button>
         </div>
