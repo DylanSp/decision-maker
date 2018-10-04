1edeaeec454129d6c68a9f3e4247cfd5c1cbd2e4
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-10-03 21:53:49 -0400

    Create and style VoteCreationButton

--- frontend/src/components/VoteCreationButton.tsx
+++ frontend/src/components/VoteCreationButton.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@material-ui/core';
 import { SFC } from 'react';
 import * as React from 'react';
 
 export const VoteCreationButton: SFC<{}> = () => {
     return (
-        <>
-        </>
+        <div style={{textAlign: "center"}}>
+            <Button variant="contained" size="large">
+                Create Vote
+            </Button>
+        </div>
     );
 }
