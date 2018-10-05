06016799099ab73a685f39cc46fb84f67b6f8228
Dylan Sprague <dylan.richard.sprague@gmail.com>
2018-10-04 20:59:24 -0400

    Wire up VoteCreationModal on LobbyScreen

--- frontend/src/components/LobbyScreen.tsx
+++ frontend/src/components/LobbyScreen.tsx
@@ -1,24 +1,40 @@
 import { Divider } from '@material-ui/core';
-import { SFC } from 'react';
 import * as React from 'react';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { VoteCreationButton } from './VoteCreationButton';
+import { VoteCreationModal } from './VoteCreationModal';
 import { VoteList } from './VoteList';
 
-export const LobbyScreen: SFC<{}> = () => {
-    return (
-        <>
-            <Header />
-            <Divider style={{
-                marginTop: "3em",
-                marginBottom: "3em",
-                marginLeft: 72,
-                marginRight: 72,
-            }}  />
-            <VoteList/>
-            <VoteCreationButton />
-            <Footer />
-        </>
-    );
+interface LobbyScreenState {
+    isVoteJoinModalOpen: boolean,
+    isVoteCreationModalOpen: boolean
+}
+
+export class LobbyScreen extends React.PureComponent<{}, LobbyScreenState> {
+    public constructor (props: {}) {
+        super(props);
+        this.state = {
+            isVoteJoinModalOpen: false,
+            isVoteCreationModalOpen: false
+        }
+    }
+
+    public render = () => {
+        return (
+            <>
+                <Header />
+                <Divider style={{
+                    marginTop: "3em",
+                    marginBottom: "3em",
+                    marginLeft: 72,
+                    marginRight: 72,
+                }}  />
+                <VoteList />
+                <VoteCreationButton onClick={() => this.setState({isVoteCreationModalOpen: true})} />
+                <VoteCreationModal isOpen={this.state.isVoteCreationModalOpen} handleClose={() => this.setState({isVoteCreationModalOpen: false})} />
+                <Footer />
+            </>
+        );
+    }
 }
--- frontend/src/components/VoteCreationModal.tsx
+++ frontend/src/components/VoteCreationModal.tsx
@@ -1,17 +1,25 @@
+import { Dialog } from '@material-ui/core';
 import { SFC } from 'react';
 import * as React from 'react';
 import { ChoiceCreationList } from './ChoiceCreationList';
 import { VoteCreationForm } from './VoteCreationForm';
 
-export const VoteCreationModal: SFC<{}> = () => {
+interface VoteCreationModalProps {
+    isOpen: boolean,
+    handleClose: () => void
+}
+
+export const VoteCreationModal: SFC<VoteCreationModalProps> = (props) => {
     return (
-        <>
-            <VoteCreationForm />
-            <ChoiceCreationList />
-            {
-                // submit button
-                // cancel button
-            }
-        </>
+        <Dialog open={props.isOpen} onClose={props.handleClose}>
+            <div>
+                <VoteCreationForm />
+                <ChoiceCreationList />
+                {
+                    // submit button
+                    // cancel button
+                }
+            </div>
+        </Dialog>
     );
 }
